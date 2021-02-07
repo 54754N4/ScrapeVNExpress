102d392501092f8da8c329449a204a8c73e018ff
Mazen Anastas <lorenzo.mallerco@gmail.com>
2021-02-07 18:09:36 +0700

    Fix: Tested on bigger comments list

--- src/main/Run.java
+++ src/main/Run.java
@@ -6,7 +6,7 @@ import scrape.CommentsScraper;
 
 public class Run {
 	public static void main(String[] args) throws IOException {
-		String url = "https://vnexpress.net/buoi-tong-duyet-le-nham-chuc-cua-biden-4223129.html";
+		String url = "https://vnexpress.net/5-gio-nha-trang-don-dep-don-tan-tong-thong-4223047.html";
 		CommentsScraper scraper = new CommentsScraper(url);
 		scraper.serialize("comments.json");
 	}
